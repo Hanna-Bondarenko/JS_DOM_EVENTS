a0d790de7a2d0caa9ea8997257114801f7c21702
Hanna Bondarenko <annaleshhuk@gmail.com>
2025-05-20 11:40:51 +0300

    Implement login form validation and data handling with custom JS logic - task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,35 @@
+// Знаходимо форму за класом
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // Забороняємо перезавантаження сторінки
+
+  const formElements = event.target.elements;
+
+  const email = formElements.email.value.trim();
+  const password = formElements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  form.reset(); // Очищення форми
+});
+
+// Пояснення, що використовувалося в коді:
+// event.target — елемент, що викликав подію (форма).
+
+// event.target.elements — колекція всіх полів форми.
+
+// .email.value і .password.value — значення інпутів за name="".
+
+// .trim() — прибирає зайві пробіли.
+
+// .reset() — очищає форму після сабміту.
